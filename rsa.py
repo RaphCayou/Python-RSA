7a982dc7e732f247311e17f7df371e4631b30ab4
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-02-22 19:40:40 -0500

    Initial code

---
+++ rsa.py
@@ -0,0 +1,20 @@
+# generer_cles() : cle_privee, cle_public
+# chiffrer(cle_public,message) : message_chiffré
+# dechiffrer(message_chiffré) : message
+
+def generate_keys():
+    private_key = ""
+    public_key = ""
+    return private_key, public_key
+
+
+def encrypt(public_key, message):
+    encrypted_message = ""
+    # return bigInt(m).modPow(e, n);
+    # return pow(bignum(m), e, n)
+    return encrypted_message
+
+
+def decrypt(encrypted_message):
+    message = ""
+    return message
