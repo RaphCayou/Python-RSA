a1a0d3514c6137edb92af657ebfc5b6f49c2ae45
jeepee12 <jeepee.12@hotmail.com>
2018-03-06 15:26:18 -0500

    Update the default value to use 256 bits key and have a bigger message.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from rsa import RSA
 my_rsa = RSA()
 
-private, public_1, public_2 = my_rsa.generate_keys(64)
-encrypt_text = my_rsa.encrypt("ABCD", public_1, public_2)
+private, public_1, public_2 = my_rsa.generate_keys(256)
+encrypt_text = my_rsa.encrypt("la clef 256 bits fonctionne a merveille", public_1, public_2)
 print(encrypt_text)
 print(my_rsa.decrypt(encrypt_text, private, public_1))
