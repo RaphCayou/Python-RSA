f1e48ff6d36a5acc02a325b0abad271f4b314492
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-03-12 13:22:07 -0400

    Removed main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from rsa import RSA
-my_rsa = RSA()
-
-private, public_1, public_2 = my_rsa.generate_keys(256)
-encrypt_text = my_rsa.encrypt("la clef 256 bits fonctionne a merveille", public_1, public_2)
-print(encrypt_text)
-print(my_rsa.decrypt(encrypt_text, private, public_1))
